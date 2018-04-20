da1ac26afdde2a05bdfd0ee9806244cd4a45cae2
Xueqi Xia <emmaxia@Xueqis-MacBook-Pro.local>
2018-04-20 16:34:39 -0700

    particiption

--- exercise-7/exercise.R
+++ exercise-7/exercise.R
@@ -1,40 +1,42 @@
 # Exercise 7: using dplyr on external data
 
 # Load the dplyr library
-
+library(dplyr)
 
 # Use the `read.csv()` function to read in the included data set. Remember to
 # save it as a variable.
-
+team_data <- read.csv("data/nba_teams_2016.csv", stringsAsFactors=FALSE)
 
 # View the data frame you loaded, and get some basic information about the 
 # number of rows/columns. 
 # Note the "X" preceding some of the column titles as well as the "*" following
 # the names of teams that made it to the playoffs that year.
-
+View(team_data)
 
 # Add a column that gives the turnovers to steals ratio (TOV / STL) for each team
-
+team_data <- mutate(team_data, Ratio = TOV / STL)
 
 # Sort the teams from lowest turnover/steal ratio to highest
 # Which team has the lowest turnover/steal ratio?
-
+team_data %>% 
+  filter(Ratio == min(Ratio)) %>% 
+  select(Team)
 
 # Using the pipe operator, create a new column of assists per game (AST / G) 
 # AND sort the data.frame by this new column in descending order.
-
+team_data <- mutate(team_data, ASTGM = AST / G) %>% arrange(-ASTGM)
 
 # Create a data frame called `good_offense` of teams that scored more than 
 # 8700 points (PTS) in the season
-
+good_offense <- filter(team_data, PTS > 8700)
 
 # Create a data frame called `good_defense` of teams that had more than 
 # 470 blocks (BLK)
-
+good_defense <- filter(team_data, BLK > 470)
 
 # Create a data frame called `offense_stats` that only shows offensive 
 # rebounds (ORB), field-goal % (FG.), and assists (AST) along with the team name.
-
+offense_stats <- select
 
 # Create a data frame called `defense_stats` that only shows defensive 
 # rebounds (DRB), steals (STL), and blocks (BLK) along with the team name.
--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -1,22 +1,22 @@
 # Exercise 8: exploring data sets
 
 # Load the dplyr library
-
+library(dplyr)
 
 # Read in the data (from `data/pupulitzer-circulation-data.csv`). Remember to 
 # not treat strings as factors!
-
+pulitzer <- read.csv("data/pulitzer-circulation-data.csv", stringsAsFactors=FALSE)
 
 # View in the data set. Start to understand what the data set contains
-
+View(pulitzer)
 
 # Print out the names of the columns for reference
-
+colnames(pulitzer)
 
 # Use the 'str()' function to also see what types of values are contained in 
 # each column (you're looking at the second column after the `:`)
 # Did any value type surprise you? Why do you think they are that type?
-
+str(pulitzer)
 
 # Add a column to the data frame called 'Pulitzer.Prize.Change` that contains 
 # the difference in the number of times each paper was a winner or finalist 
